5970ee570e18d4a05efa851ac7f97ed9bc070d57
Ioulia Kondratovitch <ik@plan2.net>
2018-06-29 10:32:32 +0200

    backend styling
    add backend styling for TextareaEditor field - make field bigger

---
+++ ext_tables.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('TYPO3_MODE') or die();
+
+// Register as a skin
+$GLOBALS['TBE_STYLES']['skins']['form_options_csv'] = [
+    'name' => 'form_options_csv',
+    'stylesheetDirectories' => [
+        'css' => 'EXT:form_options_csv/Resources/Public/Backend/css/'
+    ]
+];
