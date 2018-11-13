5f19c891a7e9be4eda5d98e441d8751d98645f9a
Wolfgang Klinger <wk@plan2.net>
2018-11-13 07:56:27 +0100

    Remove the core patch

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,9 +4,6 @@ defined('TYPO3_MODE') or die();
 
 (function () {
     if (TYPO3_MODE === 'BE') {
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Form\Controller\FormEditorController::class] = [
-            'className' => \Plan2net\FormOptionsCsv\Controller\FormEditorController::class
-        ];
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['beforeFormSave'][] = \Plan2net\FormOptionsCsv\Hooks\ImportSelectOptions::class;
     }
 })();
